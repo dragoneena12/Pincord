b5a15a521ba9592987a83eca722380b85a537768
lapi <tyoutugai.7@gmail.com>
2022-04-03 14:57:12 +0900

    add pin function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-const { Client, Intents } = require('discord.js')
+import { Client, Intents, IntentsString } from "discord.js"
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
 
-const client = new Client({ intents: Object.keys(Intents.FLAGS) })
+const client = new Client({ intents: Object.keys(Intents.FLAGS) as IntentsString[] })
 
 client.on('ready', () => {
-  console.log(`logged in as ${client.user.tag}`)
+  console.log(`logged in as ${client.user?.tag}`)
 })
 
-client.on('messageCreate', async (msg: any) => {
-  if (msg.content === '!ping') {
-    msg.channel.send('Pong!')
+client.on("messageReactionAdd", async (reaction, user) => {
+	if (reaction.emoji.name === "📌"){
+    reaction.message.pin()
   }
-})
+});
+
+client.on("messageReactionRemove", async (reaction, user) => {
+	if (reaction.emoji.name === "📌"){
+    reaction.message.unpin()
+  }
+});
 
 client.login(DISCORD_BOT_TOKEN)
