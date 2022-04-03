82b9f2e1c24049fb98dba7537021847860d1a62b
lapi <tyoutugai.7@gmail.com>
2022-04-03 14:09:53 +0900

    add ping pong

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Pincord
 Discordのピン留めするbot
+
+設定
+```
+DISCORD_BOT_TOKEN=xxxxx
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+const { Client, Intents } = require('discord.js')
 
-console.log(hello("World"));
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
+
+const client = new Client({ intents: Object.keys(Intents.FLAGS) })
+
+client.on('ready', () => {
+  console.log(`logged in as ${client.user.tag}`)
+})
+
+client.on('messageCreate', async (msg: any) => {
+  if (msg.content === '!ping') {
+    msg.channel.send('Pong!')
+  }
+})
+
+client.login(DISCORD_BOT_TOKEN)
