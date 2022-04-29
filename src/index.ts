5b3f63aeea95efde5335b55db8815dbe109e4c93
lapi <tyoutugai.7@gmail.com>
2022-04-29 11:15:34 +0900

    listen for reactions on old messages

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,48 @@
-import { Client, Intents, IntentsString } from "discord.js"
+import { Client, Intents } from "discord.js";
 
-const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 
-const client = new Client({ intents: Object.keys(Intents.FLAGS) as IntentsString[] })
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ["MESSAGE", "REACTION"],
+});
 
-client.on('ready', () => {
-  console.log(`logged in as ${client.user?.tag}`)
-})
+client.on("ready", () => {
+  console.log(`logged in as ${client.user?.tag}`);
+});
 
 client.on("messageReactionAdd", async (reaction, user) => {
-	if (reaction.emoji.name === "📌"){
-    reaction.message.pin()
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      return;
+    }
+  }
+
+  if (reaction.emoji.name === "📌") {
+    reaction.message.pin();
   }
 });
 
 client.on("messageReactionRemove", async (reaction, user) => {
-	if (reaction.emoji.name === "📌"){
-    reaction.message.unpin()
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      return;
+    }
+  }
+  
+  if (reaction.emoji.name === "📌") {
+    reaction.message.unpin();
   }
 });
 
-client.login(DISCORD_BOT_TOKEN)
+client.login(DISCORD_BOT_TOKEN);
